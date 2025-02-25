e68bc8f32408f3677dbed050b3a4e247619ee4d4
Dinursult <give.me.kitchen@gmail.com>
2025-02-25 12:29:48 +0100

    small corrections

--- ft_get_map.c
+++ ft_get_map.c
@@ -6,11 +6,19 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 12:13:42 by dias              #+#    #+#             */
-/*   Updated: 2025/02/25 12:19:03 by dias             ###   ########.fr       */
+/*   Updated: 2025/02/25 12:29:06 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+static void	ft_terminate_and_free(char *line, t_map *map)
+{
+	if (line)
+		free(line);
+	free(map);
+	ft_terminate();
+}
 
 void	ft_get_map(int fd, t_map *map)
 {
@@ -18,29 +26,17 @@ void	ft_get_map(int fd, t_map *map)
 
 	line = get_next_line(fd);
 	if (ft_linelen(line) < 3)
-	{
-		free(map);
-		free(line);
-		ft_terminate();
-	}
+		ft_terminate_and_free(line, map);
 	else
 		map->width = ft_linelen(line);
 	while (line)
 	{
 		if (ft_linelen(line) != map->width)
-		{
-			free(map);
-			free(line);
-			ft_terminate();
-		}
+			ft_terminate_and_free(line, map);
 		map->height++;
 		free(line);
 		line = get_next_line(fd);
 	}
 	if (map->height < 3 || map->width == map->height)
-	{
-		free(map);
-		free(line);
-		ft_terminate();
-	}
+		ft_terminate_and_free(line, map);
 }
--- ft_is_correct_shape.c
+++ ft_is_correct_shape.c
@@ -6,7 +6,7 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 11:42:35 by dias              #+#    #+#             */
-/*   Updated: 2025/02/25 12:14:41 by dias             ###   ########.fr       */
+/*   Updated: 2025/02/25 12:25:21 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ void	ft_is_correct_shape(char *path)
 	t_map	*map;
 
 	fd = open(path, O_RDONLY);
-	map = (t_map)malloc(sizeof(t_map));
+	if (fd == -1)
+		ft_terminate();
+	map = (t_map *)malloc(sizeof(t_map));
 	if (!map)
 		ft_terminate();
 	map->width = 0;
 	map->height = 0;
 	ft_get_map(fd, map);
+	close(fd);
 }
