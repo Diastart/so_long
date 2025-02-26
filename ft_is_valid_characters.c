b6a044b2c1866118b01639eca4686c92e74ad4da
Dinursult <give.me.kitchen@gmail.com>
2025-02-26 18:19:30 +0100

    grid validation fixed ors to ands

--- ft_is_valid_characters.c
+++ ft_is_valid_characters.c
@@ -6,7 +6,7 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 17:39:14 by dias              #+#    #+#             */
-/*   Updated: 2025/02/26 18:06:53 by dias             ###   ########.fr       */
+/*   Updated: 2025/02/26 18:14:31 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ void	ft_is_valid_characters(char **grid, t_map *map)
 		j = 0;
 		while (++j < (map->width - 1))
 		{
-			if ((grid[i][j] != '0') || \
-			(grid[i][j] != '1') || \
-			(grid[i][j] != 'C') || \
-			(grid[i][j] != 'E') || \
-			(grid[i][j] != 'P') || \
+			if ((grid[i][j] != '0') && \
+			(grid[i][j] != '1') && \
+			(grid[i][j] != 'C') && \
+			(grid[i][j] != 'E') && \
+			(grid[i][j] != 'P') && \
 			(grid[i][j] != 'B'))
 				wow(map, grid, NULL, 1);
 		}
