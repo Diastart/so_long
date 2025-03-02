e5671795c3d095f2d1becf740a73563ad02113f5
Dinursult <give.me.kitchen@gmail.com>
2025-03-02 10:44:32 +0100

    need to test

--- ft_load_images.c
+++ ft_load_images.c
@@ -6,7 +6,7 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 09:34:00 by dias              #+#    #+#             */
-/*   Updated: 2025/03/02 09:41:08 by dias             ###   ########.fr       */
+/*   Updated: 2025/03/02 10:42:17 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ void	ft_load_images(t_game *game)
 	int	img_width;
 	int	img_height;
 
-	game->img.wall = mlx_png_file_to_image(game->mlx, 
-		"textures/wall.png", &img_width, &img_height);
-	game->img.floor = mlx_png_file_to_image(game->mlx, 
-		"textures/floor.png", &img_width, &img_height);
-	game->img.collectible = mlx_png_file_to_image(game->mlx, 
-		"textures/collectible.png", &img_width, &img_height);
-	game->img.exit = mlx_png_file_to_image(game->mlx, 
-		"textures/exit.png", &img_width, &img_height);
-	game->img.player = mlx_png_file_to_image(game->mlx, 
-		"textures/player.png", &img_width, &img_height);
+	game->img.wall = mlx_xpm_file_to_image(game->mlx, 
+		"textures/wall.xpm", &img_width, &img_height);
+	game->img.floor = mlx_xpm_file_to_image(game->mlx, 
+		"textures/floor.xpm", &img_width, &img_height);
+	game->img.collectible = mlx_xpm_file_to_image(game->mlx, 
+		"textures/collectible.xpm", &img_width, &img_height);
+	game->img.exit = mlx_xpm_file_to_image(game->mlx, 
+		"textures/exit.xpm", &img_width, &img_height);
+	game->img.player = mlx_xpm_file_to_image(game->mlx, 
+		"textures/player.xpm", &img_width, &img_height);
 	
 	if (!game->img.wall || !game->img.floor || !game->img.collectible \
 	|| !game->img.exit || !game->img.player)
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 10:50:52 by dias              #+#    #+#             */
-/*   Updated: 2025/03/02 10:20:28 by dias             ###   ########.fr       */
+/*   Updated: 2025/03/02 10:37:27 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include "get_next_line.h"
-# include "minilibx_opengl/mlx.h"
+# include "minilibx-linux/mlx.h"
 # define TILE_SIZE 32
 
 typedef struct s_map
