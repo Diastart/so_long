e4f8d517afa1a3839d36fbbcd94b14a983e8069d
Dinursult <give.me.kitchen@gmail.com>
2025-03-14 16:59:33 +0100

    ...

--- ft_clean_images.c
+++ ft_clean_images.c
@@ -6,7 +6,7 @@
 /*   By: dias <dias@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 09:36:27 by dias              #+#    #+#             */
-/*   Updated: 2025/03/02 10:42:25 by dias             ###   ########.fr       */
+/*   Updated: 2025/03/04 11:10:03 by dias             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,4 @@ void	ft_clean_images(t_game *game)
 	mlx_destroy_window(game->mlx, game->win);
 	mlx_destroy_display(game->mlx);
 	free(game->mlx);
-	wow(game->map, game->grid, game->player, 1);
 }
